07df52df778496efe7eebf25b67c583371c1e18a
SkyTech <154370030+actualskytech@users.noreply.github.com>
2024-09-07 19:34:07 -0600

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -2,7 +2,7 @@
 
 echo "Updating packages..."
 sudo pacman -Syu --noconfirm
-sudo pacman -S --needed base-devel git
+sudo pacman -S --needed --noconfirm base-devel git
 
 echo "Getting config files..."
 git clone https://github.com/actualskytech/dotfiles
@@ -20,11 +20,12 @@ mkdir -p ~/.config
 cp -r dotfiles/.config/* ~/.config/
 
 echo "Installing packages..."
-sudo pacman -S hyprland rofi thunar alacritty grim slurp waybar ly bluez bluez-utils blueman 
-yay -S waypaper swaybg
+sudo pacman -S --noconfirm hyprland rofi thunar alacritty grim slurp waybar ly bluez bluez-utils blueman swappy fish 
+yay -S --noconfirm waypaper swaybg
 
 echo "Enabling services..."
 sudo systemctl enable ly.service
 sudo systemctl enable bluetooth.service
 
 echo "Done!"
+echo "Please do chsh and switch to fish"
